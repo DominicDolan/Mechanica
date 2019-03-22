a23c1fd55bb9ca87c9b11e0d52499310363960b1
Dominic Dolan <dominic.dolan@gm.com>
2019-03-22 16:55:48 +0000

    UI framework

--- src/main/java/ui/test.kt
+++ src/main/java/ui/test.kt
@@ -1,14 +1,18 @@
 package ui
+
+import loader.ioResourceToByteBuffer
+
 var i = 0
 fun main(args: Array<String>) {
-    ui {
+    val document = ui {
         div {
-
             div {
                 div {
-                    CustomDiv() {
+                    span {
                         div {
+                            div {
 
+                            }
                         }
                     }
                 }
@@ -17,35 +21,87 @@ fun main(args: Array<String>) {
                 }
             }
         }
-    }.render()
+    }
+
+    var currentDoc = document
+    var children = currentDoc.childCount
+    var siblings = 1
+
+    childRecursion(document)
 }
 
-open class DIV {
+fun childRecursion(element: TAG) : TAG? {
+    if (element.childCount == 0) return null
+    var i = 0
+    println("child")
+    while (childRecursion(element[i++]) != null) {
+    }
+
+    return childRecursion(element[element.childCount-1])
+}
+
+open class TAG(var tag: String = "") : Iterable<TAG>{
+    private var children = ArrayList<TAG>()
+    val childCount: Int
+        get() = children.size
+
     var layout: Layout? = null
     var style: Style? = null
-    fun div(block: DIV.() -> Unit) {
-        var j = i
-        var tab = ""
-        for (k in 0..j) {
-            tab += "    "
+    fun tag(block: TAG.() -> Unit) {
+        val tag = TAG()
+        children.add(tag)
+        block(TAG())
+    }
+
+    operator fun get(i: Int): TAG {
+        return children[i]
+    }
+
+
+    override fun iterator(): Iterator<TAG> {
+        return children.iterator()
+    }
+
+
+}
+
+fun TAG.div(block: TAG.() -> Unit) {
+    this.tag = "div"
+    tag(block)
+}
+
+fun TAG.span(block: TAG.() -> Unit) {
+    this.tag = "span"
+    tag(block)
+}
+
+//open class DIV : TAG("div") {
+//    fun div(block: DIV.() -> Unit) {
+//        tag {
+//            block()
+//        }
+//    }
+//}
+
+
+open class SPAN : TAG("span") {
+    fun span(block: SPAN.() -> Unit) {
+        tag {
+            block()
         }
-        println("$tab<div>")
-        i++
-        block()
-        i--
-        println("$tab</div>")
     }
 }
 
-class CustomDiv : DIV() {
-    operator fun invoke(block: DIV.() -> Unit) {
-        super.div(block)
-    }
-}
+//class CustomDiv : DIV() {
+//    fun cDiv(block: CustomDiv.() -> Unit) {
+////        div(block)
+//    }
+//}
 
-fun ui(block: DIV.() -> Unit): uiRenderer {
-    block(DIV())
-    return uiRenderer()
+fun ui(block: TAG.() -> Unit): TAG {
+    val tag = TAG()
+    block(tag)
+    return tag
 }
 
 class uiRenderer {
