e670d2846ef110563ac41b5c514edba46a810c7a
doghouse <dominic.dolan1791@gmail.com>
2024-05-13 23:32:35 +0100

    Added velocity parameter to the RenderableDouble for more accurate position estimates

--- mechanica/src/main/kotlin/com/mechanica/engine/game/delta/RenderVariable.kt
+++ mechanica/src/main/kotlin/com/mechanica/engine/game/delta/RenderVariable.kt
@@ -2,12 +2,14 @@ package com.mechanica.engine.game.delta
 
 import com.mechanica.engine.game.Game
 
-class RenderableDouble(private val getValue: () -> Double){
+class RenderableDouble(private val getValue: () -> Double, getVelocity: (() -> Double)? = null){
     var value: Double = 0.0
         private set
 
     private var lastValue: Double = getValue()
     private var currentValue: Double = getValue()
+
+    private var renderableVelocity: RenderableDouble? = if (getVelocity != null) RenderableDouble(getVelocity) else null
 
     fun update(delta: Double) {
         lastValue = currentValue
@@ -15,13 +17,14 @@ class RenderableDouble(private val getValue: () -> Double){
     }
 
     fun preRenderUpdate(accumulated: Double, delta: Double, lastFrame: Double, thisFrame: Double) {
-        val fraction = accumulated/delta
+        val velocity = renderableVelocity?.value ?: accumulated
+        val fraction = velocity/delta
         value = currentValue  + fraction*(currentValue - lastValue)
     }
 
     companion object {
-        fun create(getValue: () -> Double): RenderableDouble {
-            val double = RenderableDouble(getValue)
+        fun create(getValue: () -> Double, getVelocity: (() -> Double)? = null): RenderableDouble {
+            val double = RenderableDouble(getValue, getVelocity)
             val deltaCalculator = Game.deltaCalculator
             if (deltaCalculator !is MultiUpdateCalculator) {
                 throw IllegalStateException("Tried to use RenderableDouble but the DeltaCalculator is not an instance of MultiUpdateCalculator." +
